09846c99ff3bc939ac711beaedc82149eef2d09e
danljs <daniel.liu1973@gmail.com>
2016-09-05 17:32:31 -0400

    fix NoSuchMessageException error.
    On the registration page, click [Submit] button without inputting
    [Username], then getting the below error:
    org.springframework.context.NoSuchMessageException: No message found
    under code 'NotEmpty.userForm.username' for locale 'en_US'

--- src/main/java/com/hellokoding/auth/WebSecurityConfig.java
+++ src/main/java/com/hellokoding/auth/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.hellokoding.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,7 +21,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+    
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource rb = new ResourceBundleMessageSource();
+        rb.setBasenames(new String[] {"validation"});
+        return rb;
+    }
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
