d94ec51a944baacfd166138e97a9abfc8c3281a0
bstewart@gitlab.retail.bytes.co.za <bruce.stewart@bytes.co.za>
2018-09-09 10:44:59 +0200

    Upgrade from spring-boot 1.5.3 to 2.0.4 Modify WebSecurityConfig for spring-boot upgrade. Modify WebApplication class import of SpringBootServletInitializer for spring-boot upgrade. Change packaging from 35MB war to 22kb thin jar.

--- src/main/java/com/hellokoding/auth/WebSecurityConfig.java
+++ src/main/java/com/hellokoding/auth/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.hellokoding.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,7 +38,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
